f34b4c2888542f73a22b12e2a6977672d72aabd3
maba <mabarod25@gmail.com>
2024-12-29 13:07:36 -0800

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+
+
+int main (int argc, int argv){
+ if (argc == 1){
+    printf("wrong usage, usage is\n");
+    printf("./main type lenguage\n");
+  }
+  return 0;
+}
